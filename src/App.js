6197e2336f2eb6b43e94558bd37cc0aef40d6e0a
Alvin Cabayan <alvincabayan@gmail.com>
2019-05-09 00:42:09 +1200

    [0004-01] Added jumbotron. Known issue, image wont load.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import {Contact} from './components/Contact'
 import {NoMatch} from './components/NoMatch'
 import { Layout } from './components/Layout'
 import {NavigationBar} from './components/NavigationBar'
+import { Jumbotron } from './components/Jumbotron';
 
 function App() {
   return (
     <React.Fragment>
-      <NavigationBar/>
-      <Layout>
-        <Router>
+      <Router>
+        <NavigationBar/>
+        <Jumbotron/>
+        <Layout>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route component={NoMatch} />
           </Switch>
-        </Router>
-      </Layout>
+        </Layout>
+      </Router>
     </React.Fragment>
   );
 }
