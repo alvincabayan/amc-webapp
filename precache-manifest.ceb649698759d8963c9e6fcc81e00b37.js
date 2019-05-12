4fa027038cb0446085789ade2569e038a560b1f0
Alvin Cabayan <alvincabayan@gmail.com>
2019-05-13 01:19:40 +1200

    Updates

---
+++ precache-manifest.ceb649698759d8963c9e6fcc81e00b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae564d02286bc89c1e2520dd0aa749b",
+    "url": "/amc-webapp/index.html"
+  },
+  {
+    "revision": "c520d29bb8ceb7ed0fe5",
+    "url": "/amc-webapp/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "070bfd385ddb0d649f47",
+    "url": "/amc-webapp/static/js/2.1697b99e.chunk.js"
+  },
+  {
+    "revision": "c520d29bb8ceb7ed0fe5",
+    "url": "/amc-webapp/static/js/main.286a5595.chunk.js"
+  },
+  {
+    "revision": "e978901b99e8cd5bef23",
+    "url": "/amc-webapp/static/js/runtime~main.6d7a598b.js"
+  },
+  {
+    "revision": "056c87b8a9cfdfa3fef65860991e45fe",
+    "url": "/amc-webapp/static/media/beerImage.056c87b8.jpg"
+  }
+]);
